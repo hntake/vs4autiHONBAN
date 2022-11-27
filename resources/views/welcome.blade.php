d68a6669a384a591728e7ce9491dec8967114c0d
hntake <hntake@gmail.com>
2022-11-27 20:48:54 +0900

    11-27shortに変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -213,7 +213,7 @@
                             </div>
                             <div class="video">
                                 <video class="pc" src="img/nagi2.mp4" autoplay loop muted playsinlin></video>
-                                <video class="sp" src="img/nagi_sp3.mp4" autoplay loop muted playsinlin></video>
+                                <video class="sp" src="img/nagi_sp_short.mp4" autoplay loop muted playsinlin></video>
                             </div>
                         </div>
                         </div>
