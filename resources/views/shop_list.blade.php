1367d7a03ce7b907419c521347d85e26571132bc
hntake <hntake@gmail.com>
2023-05-06 17:04:34 +0900

    5-6list_edit

--- resources/views/shop_list.blade.php
+++ resources/views/shop_list.blade.php
@@ -30,24 +30,22 @@
     <!-- バリデーションエラーの表示 -->
     @include('common.errors')
 
-
     <h1>お守りバッジ取り扱い先一覧</h1>
     <div class="store-list">
         @foreach($shops as $shop)
         @if($shop->area=='北海道')
         <h3>北海道エリア</h3>
-
+        @endif
+        @endforeach
         <table>
-        <thead>
+        <!-- <thead>
             <tr>
                 <th style="width:10%">県</th>
                 <th style="width:20%">施設・店舗名</th>
                 <th style="width:20%">電話番号</th>
                 <th style="width:20%">メールアドレス</th>
             </tr>
-        </thead>
-        @endif
-        @endforeach
+        </thead> -->
         <tbody>
         @foreach($shops as $shop)
         @if($shop->area=='北海道')
@@ -70,14 +68,14 @@
         @if($shop->area=='東北地方')
         <h3>東北エリア</h3>
         <table>
-            <thead>
+           <!--  <thead>
                 <tr>
                     <th style="width:10%">県</th>
                     <th style="width:20%">施設・店舗名</th>
                     <th style="width:20%">電話番号</th>
                     <th style="width:20%">メールアドレス</th>
                 </tr>
-            </thead>
+            </thead> -->
         @endif
         @endforeach
             <tbody>
@@ -102,14 +100,14 @@
         @if($shop->area=='関東地方')
         <h3>関東エリア</h3>
         <table>
-            <thead>
+          <!--   <thead>
                 <tr>
                     <th style="width:10%">県</th>
                     <th style="width:20%">施設・店舗名</th>
                     <th style="width:20%">電話番号</th>
                     <th style="width:20%">メールアドレス</th>
                 </tr>
-            </thead>
+            </thead> -->
             @endif
             @endforeach
             <tbody>
@@ -130,20 +128,19 @@
             @endforeach
             </tbody>
         </table>
-        @foreach($shops as $shop)
         @if($shop->area=='中部地方')
         <h3>中部エリア</h3>
+        @endif
         <table>
-            <thead>
+           <!--  <thead>
                 <tr>
                     <th style="width:10%">県</th>
                     <th style="width:20%">施設・店舗名</th>
                     <th style="width:20%">電話番号</th>
                     <th style="width:20%">メールアドレス</th>
                 </tr>
-            </thead>
-            @endif
-            @endforeach
+            </thead> -->
+
             <tbody>
             @foreach($shops as $shop)
             @if($shop->area=='中部地方')
@@ -166,14 +163,14 @@
         @if($shop->area=='近畿地方')
         <h3>近畿エリア</h3>
         <table>
-            <thead>
+        <!--     <thead>
                 <tr>
                     <th style="width:10%">県</th>
                     <th style="width:20%">施設・店舗名</th>
                     <th style="width:20%">電話番号</th>
                     <th style="width:20%">メールアドレス</th>
                 </tr>
-            </thead>
+            </thead> -->
             @endif
             @endforeach
             <tbody>
@@ -198,14 +195,14 @@
         @if($shop->area=='中国地方')
         <h3>中国エリア</h3>
         <table>
-            <thead>
+          <!--   <thead>
                 <tr>
                     <th style="width:10%">県</th>
                     <th style="width:20%">施設・店舗名</th>
                     <th style="width:20%">電話番号</th>
                     <th style="width:20%">メールアドレス</th>
                 </tr>
-            </thead>
+            </thead> -->
             @endif
             @endforeach
             <tbody>
@@ -230,14 +227,14 @@
         @if($shop->area=='四国地方')
         <h3>四国エリア</h3>
         <table>
-            <thead>
+           <!--  <thead>
                 <tr>
                     <th style="width:10%">県</th>
                     <th style="width:20%">施設・店舗名</th>
                     <th style="width:20%">電話番号</th>
                     <th style="width:20%">メールアドレス</th>
                 </tr>
-            </thead>
+            </thead> -->
             @endif
             @endforeach
             <tbody>
@@ -262,14 +259,14 @@
         @if($shop->area=='九州沖縄地方')
         <h3>九州沖縄エリア</h3>
         <table>
-            <thead>
+          <!--   <thead>
                 <tr>
                     <th style="width:10%">県</th>
                     <th style="width:20%">施設・店舗名</th>
                     <th style="width:20%">電話番号</th>
                     <th style="width:20%">メールアドレス</th>
                 </tr>
-            </thead>
+            </thead> -->
             @endif
             @endforeach
             <tbody>
