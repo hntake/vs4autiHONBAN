14aa7fd1e7026614d250dc6804fd1a60aba67b5b
hntake <hntake@gmail.com>
2024-03-01 10:33:05 +0900

    3-1google_tag

--- resources/views/design/list.blade.php
+++ resources/views/design/list.blade.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PFPXC78X');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="UTF-8">
     <title>障がい者アートトップページ</title>
     <meta charset="UTF-8">
@@ -25,7 +32,11 @@
      crossorigin="anonymous"></script>
 </head>
 <body>
-    <!-- Twitterシェアボタン -->
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PFPXC78X"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+        <!-- Twitterシェアボタン -->
 
     <blockquote class="twitter-tweet"><p lang="en" dir="ltr"> 
         <!-- <a href="https://t.co/ffKnsVKwG4"></a> -->
