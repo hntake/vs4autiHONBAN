5de3e83f64504e6be080d697aef6136ab13fd6d4
hntake <hntake@gmail.com>
2024-03-26 11:06:46 +0900

    3-26postページエラー表示変更

--- resources/views/design/post.blade.php
+++ resources/views/design/post.blade.php
@@ -88,7 +88,7 @@
                                 </button>
                             </div>
                         </div>
-                        @if ($errors->any())
+                        <!-- @if ($errors->any())
                             <div class="alert alert-danger">
                                 <ul>
                                     @foreach ($errors->all() as $error)
@@ -96,7 +96,7 @@
                                     @endforeach
                                 </ul>
                             </div>
-                        @endif
+                        @endif -->
     </form>
 
                         </tr>
